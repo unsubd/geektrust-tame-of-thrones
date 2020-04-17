89c4068514d2497f1c8d089d156e9f2459a03aaf
Adit Pilai <aditapillai@gmail.com>
2020-04-17 11:26:25 +0530

    update test names

--- src/test/java/com/aditapillai/projects/tameofthrones/cipher/CharacterWheelTest.java
+++ src/test/java/com/aditapillai/projects/tameofthrones/cipher/CharacterWheelTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class CharacterWheelTest {
 
     @Test
-    void addTest() {
+    void moveClockwiseTest() {
         CharacterWheel instance = CharacterWheel.getInstance();
         Assertions.assertEquals('D', instance.moveClockWise('A', 3));
         Assertions.assertEquals('C', instance.moveClockWise('Z', 3));
@@ -15,7 +15,7 @@ class CharacterWheelTest {
 
 
     @Test
-    void subtract() {
+    void moveAntiClockwiseTest() {
         CharacterWheel instance = CharacterWheel.getInstance();
         Assertions.assertEquals('Z', instance.moveAntiClockwise('A', 1));
         Assertions.assertEquals('Y', instance.moveAntiClockwise('Z', 1));
