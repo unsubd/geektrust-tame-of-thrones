e383f9533945daf97a8b855db25aa7f917c72055
Adit Pilai <aditapillai@gmail.com>
2020-04-17 19:37:51 +0530

    better method names

--- src/main/java/com/aditapillai/projects/tameofthrones/computation/StringCompareUtils.java
+++ src/main/java/com/aditapillai/projects/tameofthrones/computation/StringCompareUtils.java
@@ -7,24 +7,41 @@ import java.util.stream.Collectors;
 
 public class StringCompareUtils {
 
-    public static boolean containsIndexInsensitive(String message, String emblem) {
-        Map<Character, Long> charCountMap = countMap(message);
-        return emblem.length() == emblem.chars()
-                                        .mapToObj(character -> (char) character)
-                                        .filter(character -> subtractCharacter(charCountMap, character))
-                                        .count();
+    /**
+     * Compares whether all the characters in word are present in the text
+     *
+     * @param text to be searched
+     * @param word whose characters need to be searched in the text
+     * @return true only if all the characters in the word are present in the text.
+     * <p>
+     * Example:
+     * containsIndexInsensitive("fork", "rofk") -> true
+     * containsIndexInsensitive("fork", "roofk") -> false
+     */
+    public static boolean containsIndexInsensitive(String text, String word) {
+        Map<Character, Long> charCountMap = countMap(text);
+        return word.length() == word.chars()
+                                    .mapToObj(character -> (char) character)
+                                    .filter(character -> subtractCharacterFromMap(charCountMap, character))
+                                    .count();
     }
 
-    public static Map<Character, Long> countMap(String message) {
-        return message.chars()
-                      .mapToObj(character -> (char) character)
-                      .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+    /**
+     * Given a string, return the count map for it's characters
+     *
+     * @param text text
+     * @return count map of the string
+     */
+    public static Map<Character, Long> countMap(String text) {
+        return text.chars()
+                   .mapToObj(character -> (char) character)
+                   .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
 
-    private static boolean subtractCharacter(Map<Character, Long> charMap, Character character) {
-        return charMap.compute(character, (key, value) -> Optional.ofNullable(value)
-                                                                  .map(val -> --val)
-                                                                  .orElse(-1L)) >= 0;
+    private static boolean subtractCharacterFromMap(Map<Character, Long> countMap, Character character) {
+        return countMap.compute(character, (key, value) -> Optional.ofNullable(value)
+                                                                   .map(val -> --val)
+                                                                   .orElse(-1L)) >= 0;
     }
 }
